623aa48c0ed78d05451d0c64d2d37341a9d91d29
alyuasa <yuasalicia@gmail.com>
2026-03-23 09:19:30 -0300

    mysql.js

---
+++ mysql.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql2");
+
+const pool = mysql.createConnection({
+    'user': 'root',
+    'password': 'root',
+    'database': 'postbd',
+    'host': 'localhost',
+    'port': 3307
+});
+
+exports.execute = (query, params = [], var_pool = pool) => {
+    return new Promise((resolve, reject) => {
+        var_pool.query(query, params, (error, results) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(results);
+            }
+        });
+    });
+}
